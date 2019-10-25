d4f006d33b2423e944ff20aaa3d7582eddd51ab8
bhasvara <bhasvara@cisco.com>
2019-10-26 00:06:14 +0530

    Added 2 programs to repos

---
+++ py_explore/solutions/023_get_random_strings_till_required.py
@@ -0,0 +1,16 @@
+#
+## 23. Generating random strings until a given string is generated
+#
+
+import random
+required_num = 2456
+
+count = 0
+while True:
+    random_num = random.randint(2000, 4000)
+    count     += 1
+#    print("Random number between 2000-4000 is: ", random_num)
+    if required_num == random_num:
+        print("The required random number '{}' is generated after {} iterations.".format(required_num, count))
+        break
+
