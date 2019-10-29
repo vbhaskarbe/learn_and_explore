b509e7a9b1dd0db9af9a4eacf548784662cc9027
bhasvara <bhasvara@cisco.com>
2019-10-30 03:01:27 +0530

    Finished 50th problem

--- py_explore/solutions/050_rotate_array_by_n_elements.py
+++ py_explore/solutions/050_rotate_array_by_n_elements.py
@@ -2,6 +2,8 @@
 ## 50. Python program to rotate an array by 'd' elements.
 #
 
-alist = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ]
-print("The list of elements are: ", alist)
+alist = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l' ]
+rotate_by = 3
+print("The list of elements are       :", alist)
+print("The list after rotating by {} is: {}".format( rotate_by, alist[len(alist) - rotate_by:len(alist):] + alist[0:len(alist) - rotate_by:]))
 
