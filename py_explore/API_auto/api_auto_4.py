45cf0d4a49dbea678c4411bf56719d9796952db2
bhasvara <bhasvara@cisco.com>
2019-11-20 13:16:44 +0530

    updated code a but

--- py_explore/API_auto/api_auto_4.py
+++ py_explore/API_auto/api_auto_4.py
@@ -7,18 +7,11 @@ class RestApiTester():
     def process_result( self, result):
         print( "The Response code is :", result.status_code)
         result = result.json()
-        if 'id' in result:
-            print( "Id is :",result['id'])
-        if 'body' in result:
-            print( "Text is:", result['body'])
+        print( "Id is :", result['id']) if 'id' in result else None
+        print( "Text is:", result['body']) if 'body' in result else None
         print( '%' * 80, '\n')
  
-    def get(self, url_tag):
-        full_url = self.url_home + url_tag
-        print("HTTP GET => {}".format( full_url))
-        result = requests.get( full_url)
-        self.process_result( result)
-
+    ## Wrapper func handling put, patch and delete also.
     def post( self, url_tag, data, req_type = 'post'):
         full_url = self.url_home + url_tag
         print("HTTP {} => {}".format( req_type, full_url))
@@ -26,6 +19,9 @@ class RestApiTester():
         result = http_call( url = full_url, data = data)
         self.process_result( result)
     
+    def get(self, url_tag):
+        self.post( url_tag, None, 'get')
+
     def put( self, url_tag, data):
         self.post( url_tag, data, 'put')
 
@@ -37,18 +33,18 @@ class RestApiTester():
 
 if __name__ == '__main__':
     restapi_obj = RestApiTester()
-
+    ## GET calls
     for url_tag in [ '/posts', '/posts/1', '/posts/1/comments', '/comments?postId=1', '/posts?userId=1' ]:
         restapi_obj.get( url_tag)
-
+    ## POST call with fresh payload
     p_data = {  'title' : 'foo', 'body'  : 'bar', 'userId': 1 }
     restapi_obj.post('/posts', p_data)
-
+    ## PUT call with changes to existing entity
     p_data = { 'id'    : 1, 'title' : 'new foo', 'body'  : 'new bar', 'userId': 1 }
     restapi_obj.put('/posts/1', p_data)
-
+    ## PATCH call with partial data in payload for update
     p_data = { 'title' : 'patched new title', 'body'  : 'patched new bar' }
     restapi_obj.patch('/posts/1', p_data)
-
+    ## Delete is entity
     restapi_obj.delete('/posts/1')
 
