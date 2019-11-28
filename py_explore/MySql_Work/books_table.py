df8ffc9569e7288f2c6269cbf97843023a0b24d3
bhasvara <bhasvara@cisco.com>
2019-11-28 14:30:04 +0530

    Added another table and relation link between them

--- py_explore/MySql_Work/books_table.py
+++ py_explore/MySql_Work/books_table.py
@@ -1,9 +1,10 @@
 from db_lib_methods import display_records, SQL_execute_one_statement
 
 ## Method to create books static table
-def SQL_create_books_table( db, cursor):
+def SQL_create_books_table( db, cursor, tab_name = 'book'):
+    cursor.execute("DROP TABLE IF EXISTS orders")
     cursor.execute("DROP TABLE IF EXISTS books")
-    cursor.execute("CREATE TABLE books ( \
+    cursor.execute("CREATE TABLE {tab_name} ( \
                                     id          MEDIUMINT(11) NOT NULL AUTO_INCREMENT, \
                                     title       VARCHAR(255), \
                                     description VARCHAR(255), \
@@ -11,23 +12,26 @@ def SQL_create_books_table( db, cursor):
                                     price       float(10,2), \
                                     currency    VARCHAR(20), \
                                     language    VARCHAR(25), \
+                                    in_store    TINYINT(1) NOT NULL, \
                                     category    ENUM('Vocabulory', 'Fiction', 'Academic', 'Selfhelp') NOT NULL, \
                                     PRIMARY KEY (id), \
-                                    CONSTRAINT uc_id_title UNIQUE( id, title))" )
-    cursor.execute("ALTER TABLE books AUTO_INCREMENT = 10")
-    cursor.execute("DESC books")
+                                    CONSTRAINT uc_id_title UNIQUE( id, title))".format(tab_name = tab_name) )
+    cursor.execute("ALTER TABLE books AUTO_INCREMENT = 100")
+    print("***** INFO: The schema of the table 'books'")
+    cursor.execute("DESC {tab_name}".format( tab_name = tab_name))
     print(cursor.fetchall())
     ## Defining the SQL command
-    sql_cmd = "INSERT INTO books (title, description, author, price, currency, language, category) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    sql_cmd = "INSERT INTO {tab_name} (title, description, author, price, currency, language, in_store, category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)".format(tab_name = tab_name)
     ## storing values in a variable
     values  = [
-        ("Word Power Made Easy", "Useful for competitive exams.", "Lewis Norman", '124.0', 'rs', 'English', 'Vocabulory' ),
-        ("The Alchemist", "Feel good, Enchanting novel.", "Paulo Coelho", '245.0', 'rs', 'English', 'Fiction' ),
-        ("Learning Python", "Programming languages. computers.", "Lutz Mark", '1520', 'rs', 'English', 'Academic' ), 
-        ("Asamardhuni Jeeva Yathra", "Telugu fiction novel.", "Gopi Chand", "100.0", "rs", "Telugu", "Fiction"), 
-        ("Chanakya Neeti", "Hindi self help book.", "Parashar Ashwini", "90", 'rs', 'Hindi', 'Selfhelp'),
-        ("Effective Python", "To write better python code.", "Brett Slatkin", '328.0', 'rs', 'English', 'Academic'),
-        ("Python Tricks", "Python tips and tricks.", "Dan Bader", "480", 'rs', 'English', 'Academic')
+        ("Word Power Made Easy", "Useful for competitive exams.", "Lewis Norman", '124.0', 'rs', 'English', '1', 'Vocabulory' ),
+        ("The Alchemist", "Feel good, Enchanting novel.", "Paulo Coelho", '245.0', 'rs', 'English', '1', 'Fiction' ),
+        ("Learning Python", "Programming languages. computers.", "Lutz Mark", '1520', 'rs', 'English', '1', 'Academic' ), 
+        ("Asamardhuni Jeeva Yathra", "Telugu fiction novel.", "Gopi Chand", "100.0", "rs", "Telugu", '1', "Fiction"), 
+        ("Chanakya Neeti", "Hindi self help book.", "Parashar Ashwini", "90", 'rs', 'Hindi', '1', 'Selfhelp'),
+        ("Effective Python", "To write better python code.", "Brett Slatkin", '328.0', 'rs', 'English', '1', 'Academic'),
+        ("Python Tricks", "Python tips and tricks.", "Dan Bader", "480", 'rs', 'English', '1', 'Academic'),
+        ("The Theory of Everything", "Good read about world.", "Stephen Hawking", "90", 'rs', "English", '1', "Fiction")
     ]
     ## executing the query with values
     cursor.executemany( sql_cmd, values)
@@ -35,7 +39,35 @@ def SQL_create_books_table( db, cursor):
     db.commit()
     print(cursor.rowcount, "records inserted")
     print("***** INFO: All entries from table 'books'")
-    SQL_execute_one_statement( db, cursor, "SELECT * FROM books")
+    SQL_execute_one_statement( db, cursor, "SELECT * FROM {tab_name}".format(tab_name = tab_name))
+
+def SQL_create_orders_table( db, cursor):
+    cursor.execute("DROP TABLE IF EXISTS orders")
+    cursor.execute("CREATE TABLE orders (\
+                            id          MEDIUMINT(11) NOT NULL AUTO_INCREMENT, \
+                            buyer_name  VARCHAR(255) NOT NULL, \
+                            quantity    INT(5) NOT NULL, \
+                            amount      FLOAT(11,2) NOT NULL, \
+                            book_id     MEDIUMINT(11) NOT NULL, \
+                            PRIMARY KEY (id), \
+                            FOREIGN KEY (book_id) REFERENCES books(id))")
+    print("***** INFO: The schema of the table 'orders'")
+    cursor.execute("DESC orders;")
+    print(cursor.fetchall())
+    sql_cmd = "INSERT INTO orders (buyer_name, quantity, amount, book_id) VALUES(%s, %s, %s, %s)"
+    values  = [
+                ("Bhaskar", "1", "480.0", "106"),
+                ("Bhaskar", "20", "4900", "101"),
+                ("Vishwa","2","180", "104"),
+                ("Lehit","1", "90", "107"),
+              ]
+    cursor.executemany( sql_cmd, values)
+    db.commit()
+    print(cursor.rowcount, "records inserted")
+    print("***** INFO: All entries from table 'orders'")
+    SQL_execute_one_statement( db, cursor, "SELECT * FROM orders")
+
+def SQL_learn_commands(db, cursor):
     print("***** INFO: Display only 3 entries from table 'books'")
     SQL_execute_one_statement( db, cursor, "SELECT * FROM books LIMIT 3")
     print("***** INFO: Display only 3 entries from table 'books' starting from 2nd")
@@ -70,9 +102,10 @@ if __name__ == '__main__':
     ## Get cursor
     cursor = mydb.cursor()
 
-    print("***** INFO: Create the Products type table")
-    SQL_create_books_table(mydb, cursor)
-    
+    print("***** INFO: Create the 'books' table")
+    SQL_create_books_table( mydb, cursor, 'books')
+    #SQL_learn_commands(mydb, cursor)
+    SQL_create_orders_table( mydb, cursor)
     #print( dir( mydb))
 
 
